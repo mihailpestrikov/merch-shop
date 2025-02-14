96d59ca9259d092a4feb9d199b9ef1f7414a39cd
MihailPestrikov <mihail.pestrikov4dl@gmail.com>
2025-02-15 00:04:02 +0300

    feat: add user model

---
+++ internal/models/user.go
@@ -0,0 +1,21 @@
+package models
+
+import (
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+	"time"
+)
+
+type User struct {
+	ID           uuid.UUID `gorm:"type:uuid;primaryKey;"`
+	Username     string    `gorm:"uniqueIndex;not null"`
+	PasswordHash string    `gorm:"not null"`
+	Coins        int       `gorm:"not null;default:1000"`
+	CreatedAt    time.Time
+	UpdatedAt    time.Time
+}
+
+func (u *User) BeforeCreate(tx *gorm.DB) (err error) {
+	u.ID = uuid.New()
+	return
+}
