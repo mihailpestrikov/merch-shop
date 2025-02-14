16f44b55c28d25c12082e3ee292937938cc08a71
MihailPestrikov <mihail.pestrikov4dl@gmail.com>
2025-02-15 00:01:40 +0300

    feat: add logging via zerolog

---
+++ internal/logger/logger.go
@@ -0,0 +1,29 @@
+package logger
+
+import (
+	"Avito-backend-trainee-assignment-winter-2025/config"
+	"github.com/rs/zerolog"
+	"os"
+)
+
+func InitLogger(cfg *config.Config) *zerolog.Logger {
+	levelStr := cfg.LogLevel
+	format := cfg.LogFormat
+
+	level, err := zerolog.ParseLevel(levelStr)
+	if err != nil {
+		level = zerolog.InfoLevel
+	}
+
+	var logger zerolog.Logger
+	if format == "text" {
+		output := zerolog.ConsoleWriter{Out: os.Stdout}
+		logger = zerolog.New(output).Level(level).With().Timestamp().Logger()
+	} else {
+		logger = zerolog.New(os.Stdout).Level(level).With().Timestamp().Logger()
+	}
+
+	logger.Info().Msg("Logger initialized")
+
+	return &logger
+}
