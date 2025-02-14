5512a43a6bf4013d3f0bb9a4d2a3f9a242f5b805
MihailPestrikov <mihail.pestrikov4dl@gmail.com>
2025-02-15 00:03:55 +0300

    feat: add transaction model

---
+++ internal/models/transaction.go
@@ -0,0 +1,23 @@
+package models
+
+import (
+	"github.com/google/uuid"
+	"time"
+)
+
+type TransactionType string
+
+const (
+	TransactionTypeTransfer TransactionType = "transfer"
+	TransactionTypePurchase TransactionType = "purchase"
+)
+
+type Transaction struct {
+	ID          uuid.UUID       `gorm:"type:uuid;primaryKey;"`
+	Type        TransactionType `gorm:"not null"`
+	FromUserID  uuid.UUID       `gorm:"type:uuid;not null"`
+	ToUserID    uuid.UUID       `gorm:"type:uuid;not null"`
+	Amount      int             `gorm:"not null"`
+	MerchItemID *uuid.UUID      `gorm:"type:uuid"`
+	CreatedAt   time.Time       `gorm:"not null"`
+}
