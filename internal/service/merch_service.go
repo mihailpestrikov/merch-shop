68e4c234705d672da9f618da9e848edbcb034c90
MihailPestrikov <mihail.pestrikov4dl@gmail.com>
2025-02-15 21:38:23 +0300

    feat: implemented merch service

--- internal/service/merch_service.go
+++ internal/service/merch_service.go
@@ -2,80 +2,82 @@ package service
 
 import (
 	"Avito-backend-trainee-assignment-winter-2025/internal/models"
-	"errors"
-	"github.com/google/uuid"
+	"Avito-backend-trainee-assignment-winter-2025/repository"
 	"github.com/rs/zerolog"
-	"gorm.io/gorm"
 )
 
 type MerchService interface {
 	GetAllMerchItems() ([]models.MerchItem, error)
-	GetMerchItemByID(merchItemID uuid.UUID) (*models.MerchItem, error)
+	GetMerchItemByName(merchItemName string) (*models.MerchItem, error)
 	AddMerchItem(item models.MerchItem) error
-	UpdateMerchItem(merchItemID uuid.UUID, item models.MerchItem) error
-	DeleteMerchItem(merchItemID uuid.UUID) error
+	UpdateMerchItem(merchItemName string, item models.MerchItem) error
+	DeleteMerchItem(merchItemName string) error
 	InitMerchItems() error
 }
 
 type MerchServiceImpl struct {
-	db  *gorm.DB
-	log *zerolog.Logger
+	repo repository.MerchRepository
+	log  *zerolog.Logger
 }
 
-func NewMerchServiceImpl(db *gorm.DB, log *zerolog.Logger) MerchService {
-	return &MerchServiceImpl{db: db, log: log}
-}
-
-var merchCatalog = []*models.MerchItem{
-	{Name: "t-shirt", Price: 80},
-	{Name: "cup", Price: 20},
-	{Name: "book", Price: 50},
-	{Name: "pen", Price: 10},
-	{Name: "powerbank", Price: 200},
-	{Name: "hoody", Price: 300},
-	{Name: "umbrella", Price: 200},
-	{Name: "socks", Price: 10},
-	{Name: "wallet", Price: 50},
-	{Name: "pink-hoody", Price: 500},
+func NewMerchService(repo repository.MerchRepository, log *zerolog.Logger) MerchService {
+	return &MerchServiceImpl{repo: repo, log: log}
 }
 
 func (s *MerchServiceImpl) GetAllMerchItems() ([]models.MerchItem, error) {
-	//TODO implement me
-	panic("implement me")
+	items, err := s.repo.GetAll()
+	if err != nil {
+		s.log.Error().Err(err).Msg("Failed to get all merch items")
+		return nil, err
+	}
+	return items, nil
 }
 
-func (s *MerchServiceImpl) GetMerchItemByID(merchItemID uuid.UUID) (*models.MerchItem, error) {
-	//TODO implement me
-	panic("implement me")
+func (s *MerchServiceImpl) GetMerchItemByName(merchItemName string) (*models.MerchItem, error) {
+	item, err := s.repo.GetByName(merchItemName)
+	if err != nil {
+		s.log.Error().Err(err).Msgf("Failed to get merch item by name: %s", merchItemName)
+		return nil, err
+	}
+	return item, nil
 }
 
 func (s *MerchServiceImpl) AddMerchItem(item models.MerchItem) error {
-	//TODO implement me
-	panic("implement me")
+	err := s.repo.CreateMerch(&item)
+	if err != nil {
+		s.log.Error().Err(err).Msgf("Failed to add merch item: %v", item)
+		return err
+	}
+	s.log.Info().Msgf("Merch item added: %v", item)
+	return nil
 }
 
-func (s *MerchServiceImpl) UpdateMerchItem(merchItemID uuid.UUID, item models.MerchItem) error {
-	//TODO implement me
-	panic("implement me")
+func (s *MerchServiceImpl) UpdateMerchItem(merchItemName string, item models.MerchItem) error {
+	err := s.repo.UpdateMerch(merchItemName, &item)
+	if err != nil {
+		s.log.Error().Err(err).Msgf("Failed to update merch item with name: %s", merchItemName)
+		return err
+	}
+	s.log.Info().Msgf("Merch item updated: %v", item)
+	return nil
 }
 
-func (s *MerchServiceImpl) DeleteMerchItem(merchItemID uuid.UUID) error {
-	//TODO implement me
-	panic("implement me")
+func (s *MerchServiceImpl) DeleteMerchItem(merchItemName string) error {
+	err := s.repo.DeleteMerch(merchItemName)
+	if err != nil {
+		s.log.Error().Err(err).Msgf("Failed to delete merch item with name: %s", merchItemName)
+		return err
+	}
+	s.log.Info().Msgf("Merch item deleted with name: %s", merchItemName)
+	return nil
 }
 
 func (s *MerchServiceImpl) InitMerchItems() error {
-	for _, item := range merchCatalog {
-		var existing models.MerchItem
-		err := s.db.Where("name = ?", item.Name).First(&existing).Error
-
-		if errors.Is(err, gorm.ErrRecordNotFound) {
-			if err := s.db.Create(item).Error; err != nil {
-				s.log.Error().Err(err).Msgf("Error creating merch item: %s", item.Name)
-				return err
-			}
-			s.log.Info().Msgf("Merch item %s added to DB", item.Name)
-		}
+	err := s.repo.InitMerchItems()
+	if err != nil {
+		s.log.Error().Err(err).Msg("Failed to initialize merch items")
+		return err
 	}
+	s.log.Info().Msg("Merch items initialized")
 	return nil
 }
